5f9e9d4469f283a05424636ec2e8333250aab2e7
JuanRaV <juanravirgen@gmail.com>
2023-10-04 19:27:06 -0600

    Configuring head

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,7 @@
-import Head from 'next/head'
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&family=Outfit:wght@400;700;900&display=swap" rel="stylesheet"/>
-      </Head>
       <h1>Hola mundo desde Next</h1>
     </>
   )
