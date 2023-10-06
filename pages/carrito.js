06a5d0d0d51ab4830471837ae3694e8bdb911ed3
JuanRaV <juanravirgen@gmail.com>
2023-10-06 17:06:03 -0600

    Adding a form for each guitar with the number of ones thas wants

---
+++ pages/carrito.js
@@ -0,0 +1,26 @@
+import Layout from '../components/layout'
+import styles from '../styles/carrito.module.css'
+
+const Carrito = () => {
+  return (
+    <Layout
+        title="Carrito de Compras"
+    >
+        <main className='contenedor'>
+            <h1 className='heading'>Carrito</h1>
+
+            <div className={styles.contenido}>
+                <div className={styles.carrito}>
+                    <h2>Articulos</h2>
+                </div>
+                <aside className={styles.resumen}>
+                    <h3>Resumen del Pedido</h3>
+                    <p>Total a pagar: </p>
+                </aside>
+            </div>
+        </main>
+    </Layout>
+  )
+}
+
+export default Carrito
